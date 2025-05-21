85aca9e2c4d359e069ca66a6c48f34c2e51979bb
FarmerSamuel-FS <sfarmer1@student.fullsail.edu>
2025-05-20 23:52:10 -0400

    Week 2 log.md fix

--- App/iom-backend/config/db.js
+++ App/iom-backend/config/db.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI); // Corrected key
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(`MongoDB connection error: ${err.message}`);
+    console.error(`❌ MongoDB connection error: ${err.message}`);
     process.exit(1);
   }
 };
